e488e5053acb983b1d5f34bf013444cf2f5ec78e
salaremillat1234-coder <salaremillat1234@gmail.com>
2026-01-20 00:08:33 +0530

    Add files via upload
    [Add Task 1 and Task 2 Python scripts]

---
+++ Task1_math_operations .py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
